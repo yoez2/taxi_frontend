fd91228c282aa71326cb2934e29113060d6cc349
yoezer <dawayoezer22@gmail.com>
2025-05-28 18:19:34 +0600

    Fix: Made Navbar and Footer components standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
-
-import { RouterModule, Routes } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { HomepageComponent } from '../pages/homepage/homepage.component';
-
-
-import { NgModule } from '@angular/core';
 import { AboutusComponent } from '../pages/aboutus/aboutus.component';
 import { CustomerComponent } from '../pages/customer/customer.component';
 import { TouristComponent } from '../pages/tourist/tourist.component';
@@ -12,51 +7,38 @@ import { TaxiComponent } from '../pages/taxi/taxi.component';
 import { UpdateCustomerComponent } from '../pages/customer/update-customer/update-customer.component';
 import { UpdateTouristComponent } from '../pages/tourist/update-tourist/update-tourist.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
-    path:'',
-    redirectTo:'home',
-    pathMatch:'full'
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path:'home',
+    path: 'home',
     component: HomepageComponent
   },
-
   {
-    path:'about us',
+    path: 'about us',
     component: AboutusComponent
   },
   {
-    path:'customer',
+    path: 'customer',
     component: CustomerComponent
   },
   {
-    path:'tourist',
+    path: 'tourist',
     component: TouristComponent
   },
- 
   {
-    path:'taxi',
+    path: 'taxi',
     component: TaxiComponent
   },
-
   {
-    path: 'customer/update/:id', 
+    path: 'customer/update/:id',
     component: UpdateCustomerComponent
   },
   {
-    path:'tourist/update/:id',
+    path: 'tourist/update/:id',
     component: UpdateTouristComponent
   }
-  
-  
-  
-  
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
